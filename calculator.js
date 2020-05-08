503eabe4b2638e499290cecefb56179795ea659a
NaGoo <nagucsengg@gmail.com>
2020-05-08 14:26:00 +0530

    add JS file

---
+++ calculator.js
@@ -0,0 +1,29 @@
+let add=(a,b)=>
+{
+    let c=a+b
+    return c
+}
+
+let sub=(a,b)=>
+{
+    let c=a-b
+    return c
+}
+let mul=(a,b)=>
+{
+    let c=a*b
+    return c
+}
+let div=(a,b)=>
+{
+    let c=a/b
+    return c
+}
+let addition=add(2,6)
+let subtraction=sub(8,6)
+let multiplication=mul(5,6)
+let division=div(15,3)
+console.log('Addition of two no is :', addition)
+console.log('subtraction of two no is :', subtraction)
+console.log('multiplication of two no is :', multiplication)
+console.log('division of two no is :', division)
